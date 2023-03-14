8533fb3aa3def35e55c06272f2a4db337123fa03
Ryan Eastman <rking1323@gmail.com>
2023-03-14 03:17:19 +0000

    fixed challenge 5 to pass test

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -86,7 +86,7 @@ const addValues = (arr, value) => {
 
 const addNumbers = (num, arr, times, callback) => {
   for(let i = 0; i < times; i++) {
-    callback(num, arr);
+    callback(arr, num);
   }
   return arr;
 };
