ccc196ad578219464680144253ae313bb9e29328
Ryan Eastman <rking1323@gmail.com>
2023-06-03 14:22:56 -0700

    wrote out binary search tree, moving on to tests

---
+++ javascript/code-challenges/trees/trees.test.js
@@ -0,0 +1,21 @@
+`use strict`;
+
+// Code Challenge 15: Trees Tests
+
+// Can successfully instantiate an empty tree
+
+// Can successfully instantiate a tree with a single root node
+
+// For a Binary Search Tree, can successfully add a left child and right child properly to a noe
+
+// Can successfully return a collection from a pre-order traversal
+
+// Can successfully return a collection from an in-order traversal
+
+// Can successfully return a collection from a post-order traversal
+
+// Returns False for the contains method, given an existing or non-existing node value
+
+// Returns True for the contains method, given an existing node value
+
+
